9e6ea58522de846daf16407e76c08b199403dda8
de1ux <nathan@de1ux.com>
2022-02-24 15:31:20 -0700

    Fix promises on deal

--- script.js
+++ script.js
@@ -5,12 +5,11 @@ function deal() {
     axios.get("http://127.0.0.1:8000/api/game/234")
         .then(d => d.data)
         .then(data => {
-            return data;
+            let player_cards = document.querySelector("#player_cards");
+            player_cards.innerHTML = data.card;
         })
 }
 
 let hitButton = document.querySelector("#hit");
 hitButton.onclick = () => deal();
 
-let player_cards = document.querySelector("#player_cards");
-player_cards.innerHTML = deal();
