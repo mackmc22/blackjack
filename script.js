6ce74cea32ad43a5e031f03d483e4890992334f2
mackmc22 <mackmc22@gmail.com>
2022-02-28 18:46:50 -0500

    added restart button

--- script.js
+++ script.js
@@ -6,10 +6,18 @@ function deal() {
         .then(d => d.data)
         .then(data => {
             let player_cards = document.querySelector("#player_cards");
-            player_cards.innerHTML = data.card;
+            player_cards.innerHTML = data.hand;
         })
 }
 
 let hitButton = document.querySelector("#hit");
 hitButton.onclick = () => deal();
 
+
+function restart(){
+    axios.get("http://127.0.0.1:8000/api/game/234")
+        )
+let restartButton = document.querySelector("#restart");
+restartButton.onclick = () => restart_game();
+
+}
