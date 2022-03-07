c7f54b0e4ba2730408b21517f70ac48d2535a17f
mackmc22 <mackmc22@gmail.com>
2022-03-07 18:09:12 -0500

    added calculate_cards function code, updated deal endpoint to return score, updated script and html files to display score on player page

--- script.js
+++ script.js
@@ -2,11 +2,13 @@
 
 
 function deal() {
-    axios.get("http://127.0.0.1:8000/api/game/234")
+    axios.get("http://127.0.0.1:8000/api/game/234/deal")
         .then(d => d.data)
         .then(data => {
             let player_cards = document.querySelector("#player_cards");
             player_cards.innerHTML = data.hand;
+            let player_score = document.querySelector("#player_score")
+            player_score.innerHTML = data.score;
         })
 }
 
@@ -14,10 +16,10 @@ let hitButton = document.querySelector("#hit");
 hitButton.onclick = () => deal();
 
 
-function restart(){
-    axios.get("http://127.0.0.1:8000/api/game/234")
-        )
+function restart_game(){
+    axios.put("http://127.0.0.1:8000/api/game/234/restart")
+    }
 let restartButton = document.querySelector("#restart");
 restartButton.onclick = () => restart_game();
 
-}
+
