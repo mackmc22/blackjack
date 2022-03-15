ff2a27a1fb038ede480c01ae85297cb4dd6a1b81
mackmc22 <mackmc22@gmail.com>
2022-03-15 17:51:35 -0400

    updated some html and script code for the stand buttone/endpoint

--- script.js
+++ script.js
@@ -9,6 +9,10 @@ function deal() {
             player_cards.innerHTML = data.hand;
             let player_score = document.querySelector("#player_score")
             player_score.innerHTML = data.score;
+            let player_outcome = document.querySelector("#player_outcome")
+            player_outcome.innerHTML = data.score;
+            let dealer_outcome = document.querySelector("#dealer_outcome")
+            dealer_outcome.innerHTML = data.score;
         })
 }
 
@@ -23,3 +27,14 @@ let restartButton = document.querySelector("#restart");
 restartButton.onclick = () => restart_game();
 
 
+function stand(){
+    axios.get("http://127.0.0.1:8000/api/game/234/stand")
+        .then(d => d.data)
+        .then(data => {
+            let dealer_cards = document.querySelector("#dealer_cards");
+            dealer_cards.innerHTML = data.hand;
+            player_outcome.innerHTML = data.score;
+            let dealer_outcome = document.querySelector("#dealer_outcome")
+            dealer_outcome.innerHTML = data.score;
+        })
+}
